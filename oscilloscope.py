77051d09eed119790c60d20cd01fe14e9ad47929
tt-lab-general <100188261+tt-lab-general@users.noreply.github.com>
2023-01-27 16:19:30 +0900

    Successed the recording

--- oscilloscope.py
+++ oscilloscope.py
@@ -2,6 +2,10 @@ import pyvisa
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
+
+def binaryToDecimal(n):
+    return int(n, 2)
 
 try:
     rm = pyvisa.ResourceManager()
@@ -12,28 +16,49 @@ except:
     sys.exit(0)
 osci.timeout = 30000
 
-CHAN = 2
-SAMPLE_RATE = osci.query('SARA?')
-SAMPLE_RATE = SAMPLE_RATE[len('SARA '):-5]
-SAMPLE_RATE = float(SAMPLE_RATE)
-FS = 1e9
+fs = osci.query('SARA?')
+fs = fs[len('SARA '):-5]
+fs = float(fs)
+print(fs)
 
-# osci.write('*RST')
-# osci.write(':STOP')
+vd = osci.query('C2:VDIV?')
+vd = vd[len('C2:VDIV '):-2]
+vd = float(vd)
+print(vd)
+
+voff = osci.query('C2:OFST?')
+voff = voff[len('C2:OFST '):-2]
+voff = float(voff)
+print(voff)
+
+osci.write(':STOP')
 osci.write('WFSU SP, 1, NP, 0, FP, 0')
 osci.write('C2:WF? DAT2')
-# osci.chunk_size = 1024**3
+osci.chunk_size = 1024**3
 
-WAVEFORM = osci.query_binary_values('C2:WF? DAT2', datatype='i', is_big_endian=True)
-# WAVEFORM = osci.query_binary_values('C2:WF? DAT2')
+osci.write('CSVS_SAVE SAVE, OFF')
+osci.write('GET_CSV? SAVE, ON')
+osci.write('*SAV')
+osci.write('SCSV YES')
 
-X = np.arange(0, len(WAVEFORM), 1)
-TIME = X/FS
-print(len(X))
-# osci.write('FLNM')
-# osci.write('ACQuire:STATE STOP')
+wave = osci.query_binary_values('C2:WF? DAT2', datatype='b', is_big_endian=True)
+# print(f'test: {wave[0]}')
 
-plt.plot(TIME, WAVEFORM, color='black')
+wave = list(map(float, wave))
+wave = np.array(wave)
+v = wave * (vd / 25) - voff
+
+x = np.arange(0, len(wave), 1)
+t = x/fs
+
+#csv
+save_csv = np.c_[t, v]
+np.savetxt('data/sample.csv', save_csv, delimiter=',')
+
+#Figure
+plt.plot(t*1e6, v*1e3, color='black')
+plt.xlabel('Time (\u03bcs)')
+plt.ylabel('Voltage (mV)')
 plt.show()
 
 
